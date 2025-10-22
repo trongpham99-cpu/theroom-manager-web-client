9cbfc7bed63bf16964277fc8aaabc02bd70eb3e6
An Tran <antt@Ans-MacBook-Air.local>
2025-10-22 20:28:54 +0700

    fix: login page modyfying

--- src/app/(public)/(auth)/components/ui/SignInPageTitle.tsx
+++ src/app/(public)/(auth)/components/ui/SignInPageTitle.tsx
@@ -1,27 +1,20 @@
 import Typography from '@mui/material/Typography';
 import Link from '@fuse/core/Link';
-import Box from '@mui/material/Box';
 
 function SignInPageTitle() {
 	return (
 		<div className="w-full">
-			<Box
-				component="img"
-				src="/assets/images/logo/logo.png"
+			<img
+				className="w-12"
+				src="/assets/images/logo/logo.svg"
 				alt="logo"
-				sx={{
-					width: '64px',
-					'@media (min-width: 700px) and (max-width: 1210px)': {
-						display: 'none'
-					}
-				}}
 			/>
 
-			<Typography className="mt-8 text-5xl leading-[1.25] font-extrabold tracking-tight">Sign in</Typography>
-			<div className="mt-2 flex items-baseline font-medium text-lg">
-				<Typography className="text-lg">Don't have an account?</Typography>
+			<Typography className="mt-8 text-4xl leading-[1.25] font-extrabold tracking-tight">Sign in</Typography>
+			<div className="mt-0.5 flex items-baseline font-medium">
+				<Typography>Don't have an account?</Typography>
 				<Link
-					className="ml-1 text-lg"
+					className="ml-1"
 					to="/sign-up"
 				>
 					Sign up
